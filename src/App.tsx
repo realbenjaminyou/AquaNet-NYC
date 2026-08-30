721c616ef1e53672c4c71a0c0495e40f7f4e5f37
Benjamin You <63015869+realbenjaminyou@users.noreply.github.com>
2026-08-30 16:12:30 -0400

    conductor-sync-2026-08-30--20-12-30

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-dotgrid-glow flex items-center justify-center">
+      <p className="text-md text-text-muted tracking-wide select-none">
+        Your app will be here
+      </p>
+    </div>
+  );
+}
